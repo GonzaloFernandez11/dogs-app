1cbb785429ed3a3506791a5123c69ad716268aad
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-02-01 19:20:14 -0300

    configuracion 2

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, '0.0.0.0', () => {
-    console.log(`%s listening at port ${PORT}`); // eslint-disable-line no-console
+    console.log(`%s listening at port 0.0.0.0 ${PORT}`); // eslint-disable-line no-console
   });
 });
